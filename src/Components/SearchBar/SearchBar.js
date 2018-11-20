1a81eabea7b47e3d82ed0e7c3645aaf7a5bcf80f
Teched25 <cindy12201836@gmail.com>
2018-11-19 20:01:29 -0500

    Add files via upload

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    };
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  // Sets the argument for the search method in App.js to the state of term
+  search() {
+    this.props.onSearch(this.state.term);
+  }
+
+  // Sets the state of term to the value the user enters in the input
+  // handleTermChange method
+  handleTermChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  handleKeyPress(event) {
+    if(event.key === 'Enter') {
+      this.search();
+    }
+  }
+
+  render() {
+    return(
+      <div className="SearchBar">
+        <input  placeholder="Enter A Song, Album, or Artist"
+                onChange={this.handleTermChange}
+                onKeyPress={this.handleKeyPress} />
+        <a  onClick={this.search}>SEARCH</a>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './TrackList.css';
+import Track from '../Track/Track';
+
+class TrackList extends React.Component {
+// creates a component
+
+    render() {
+        return(
+            <div className="TrackList">
+                {
+                    this.props.tracks.map(track => {
+                        return <Track   key={track.id}
+                                        track={track}
+                                        onAdd={this.props.onAdd}
+                                        onRemove={this.props.onRemove}
+                                        isRemoval={this.props.isRemoval}
+                                        tracks={this.props.tracks}/>
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default TrackList;
